9c17aba685f32be35fd0129db21e83f7cf849fb4
LillaKoncz <koncz.lilla92@gmail.com>
2023-10-05 13:03:34 +0200

    try again

--- src/components/details/PizzaDetails.jsx
+++ src/components/details/PizzaDetails.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useParams, useNavigate } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import PIZZAS from '../../assets/PIZZA'
 import HAMBURGERS from '../../assets/HAMBURGER'
 import POMMES from '../../assets/POMMES'
