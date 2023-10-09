4a07c10cceba52bdf8b9cd1920da01e333b057a3
LillaKoncz <koncz.lilla92@gmail.com>
2023-10-09 16:05:43 +0200

    display cart item

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {Nav} from '../nav/Nav'
 import { CartItem } from './CartItem'
+import FOOD from '../../assets/FOOD'
+import { ShopContext } from '../shop-context/ShopContext'
+import './Cart.css'
+
 
 
 export const Cart = () => {
+  const {addToCart, cart} = useContext(ShopContext); 
 
+ 
   return (
     <>
     <Nav />
@@ -12,7 +18,11 @@ export const Cart = () => {
       <div className='cart-title'><h1>Your cart</h1></div>
 
       <div className='cart-items'> {/*flexbox nev es ar egymas mellett*/}
-        <div>Cart items </div>
+        <div className='cart-items'>{FOOD.map((food) => {
+          if(cart[food.id] !== 0) {
+             return <CartItem data={food} key={food.id} food={food}/>
+          }
+        })}</div>
         <div>Cart items price</div>
       </div>
 
--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import './Cart.css'
 
-export const CartItem = () => {
+
+export const CartItem = (props) => {
+  const {id, name, image, price, description} = props.data;
+
   return (
-    <div>CartItem</div>
+    <div className='cart-item'>
+      <img src={image} width='80px'/>
+       <div><h5 className='item-name'>{name}</h5></div> 
+       <div><h5 className='item-price'>{price} DKK</h5></div> 
+     </div>
   )
 }
